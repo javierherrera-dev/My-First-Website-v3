9037576ce6389dfca8c9a7e09cf3d95170a55430
javierherrera-dev <80907861+javierherrera-dev@users.noreply.github.com>
2023-03-02 21:29:26 -0500

    Added API and such

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 JOBS = [
     {
      "id" : 1,
-        "title" : "Business Analyst",
+        "title" : "Bus. Analyst",
         "location" : "Charlotte, NC"
     },
       {
@@ -31,6 +31,10 @@ JOBS = [
 @app.route("/")
 def hello_world():
     return render_template("home.html", jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5001, debug=True)
