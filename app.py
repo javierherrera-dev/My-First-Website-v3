c15916457ab9991e85f188b10e8442f17ac4f142
javierherrera-dev <80907861+javierherrera-dev@users.noreply.github.com>
2023-03-02 20:44:39 -0500

    commit

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+     "id" : 1,
+        "title" : "Business Analyst",
+        "location" : "Charlotte, NC"
+    },
+      {
+        "id" : 2,
+        "title" : "Manager",
+        "company" : "EY",
+        "location" : "Chicago"
+    },
+      {
+        "id" : 3,
+        "title" : "Specialist",
+        "company" : "McKinsey & Company",
+        "location" : "Charlotte"
+    },
+      {
+        "id" : 4,
+        "title" : "Operations Manager",
+        "company" : "Shopify",
+        "location" : "Charlotte"
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html", jobs=JOBS)
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5001, debug=True)
